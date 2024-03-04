f6979bab9ddbeb08435431cba234ca78f2440629
yaraaos <yara144ss@gmail.com>
2024-03-04 03:51:00 +0200

    corrected

--- Task 2/bot.py
+++ Task 2/bot.py
@@ -3,24 +3,24 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, *args
 
-def add_username_phone(args, contacts):
+def add_contact(args, contacts):
     username, phone = args
     contacts[username] = phone
     return "Contact added."
 
-def change_username_phone(args, contacts):
+def change_contact(args, contacts):
     phone, username = args
     contacts[phone] = username
     return "Phone changed."
 
-def phone_username(args, contacts):
+def show_phone(args, contacts):
     username = args[0]
     if username in contacts:
         return contacts[username]
     else:
         return "Contact not found"
 
-def all_contacts(contacts):
+def show_all(contacts):
     for username, phone in contacts.items():
         print(f"Username: {username}, Phone: {phone}")
     
@@ -40,13 +40,13 @@ def main():
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(add_username_phone(args, contacts))
+            print(add_contact(args, contacts))
         elif command == "change":
-            print(change_username_phone(args, contacts))
+            print(change_contact(args, contacts))
         elif command == "all":
-            all_contacts(contacts)
+            show_all(contacts)
         elif command == "call":
-            print(phone_username(args, contacts))
+            print(show_phone(args, contacts))
         else:
             print("Invalid command.")
 
